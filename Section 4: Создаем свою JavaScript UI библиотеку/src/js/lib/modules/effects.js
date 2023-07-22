bc8865113532777a8ab6dd2bf2f411db7d31d439
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-07-22 23:23:36 +0300

    Creating the fadeToggle animation

--- Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/effects.js
+++ Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/effects.js
@@ -1,10 +1,10 @@
-import $  from "../core";
+import $ from '../core';
 
-$.prototype.animeteOverTime = function(dur, cb, fin){
+$.prototype.animateOverTime = function(dur, cb, fin) {
     let timeStart;
 
-    function _animeteOverTime(time){
-        if(!timeStart){
+    function _animateOverTime(time) {
+        if (!timeStart) {
             timeStart = time;
         }
 
@@ -13,49 +13,73 @@ $.prototype.animeteOverTime = function(dur, cb, fin){
 
         cb(complection);
 
-        if(timeElapsed < dur){
-            requestAnimationFrame(animeteOverTime)
-        }else{
-            if(typeof fin === 'function'){
+        if (timeElapsed < dur) {
+            requestAnimationFrame(_animateOverTime);
+        } else {
+            if (typeof fin === 'function') {
                 fin();
             }
         }
     }
 
-    return _animeteOverTime;
+    return _animateOverTime;
 };
 
-$.prototype.fideIn = function(dur, display, fin){
-    for(let i = 0; i < this.length; i++){
+$.prototype.fadeIn = function(dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
         this[i].style.display = display || 'block';
 
-        const _fideIn = (complection ) =>{
+        const _fadeIn = (complection) => {
             this[i].style.opacity = complection;
         };
 
-
-        const ani = this.animeteOverTime(dur, _fideIn, fin);
+        const ani = this.animateOverTime(dur, _fadeIn, fin);
         requestAnimationFrame(ani);
     }
-    
+
     return this;
-}
+};
 
-$.prototype.fideOut = function(dur, display, fin){
-    for(let i = 0; i < this.length; i++){
-       
-
-        const _fideOut = (complection ) =>{
+$.prototype.fadeOut = function(dur, fin) {
+    for (let i = 0; i < this.length; i++) {
+        
+        const _fadeOut = (complection) => {
             this[i].style.opacity = 1 - complection;
-            if (complection === 1){
+            if (complection === 1) {
                 this[i].style.display = 'none';
             }
         };
 
-
-        const ani = this.animeteOverTime(dur, _fideOut, fin);
+        const ani = this.animateOverTime(dur, _fadeOut, fin);
         requestAnimationFrame(ani);
     }
-    
+
     return this;
-}
+};
+
+$.prototype.fadeToggle = function(dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === 'none') {
+            this[i].style.display = display || 'block';
+
+            const _fadeIn = (complection) => {
+                this[i].style.opacity = complection;
+            };
+    
+            const ani = this.animateOverTime(dur, _fadeIn, fin);
+            requestAnimationFrame(ani);
+        } else {
+            const _fadeOut = (complection) => {
+                this[i].style.opacity = 1 - complection;
+                if (complection === 1) {
+                    this[i].style.display = 'none';
+                }
+            };
+    
+            const ani = this.animateOverTime(dur, _fadeOut, fin);
+            requestAnimationFrame(ani);
+        }
+    }
+
+    return this;
+};
