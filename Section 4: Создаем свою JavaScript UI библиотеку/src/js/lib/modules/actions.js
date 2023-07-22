cedfd75e95b1c2c7f0a4ddd8e80f31c64dd47bb9
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-07-22 14:04:27 +0300

    Creating methods for working with elements, part 1

--- Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/actions.js
+++ Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/actions.js
@@ -1,35 +1,66 @@
-import $  from "../core"; 
+import $  from "./core";
 
-$.prototype.on  = function(eventName, callbeck){
-    if(!eventName || !callbeck){
-        return this;
-    }
-    for (let i = 0; i < this.length; i++){
-        this[i].addEventListener(eventName, callbeck);
-    }
-
-    return this;
-}
-
-$.prototype.off  = function(eventName, callbeck){
-    if(!eventName || !callbeck){
-        return this;
-    }
-    for (let i = 0; i < this.length; i++){
-        this[i].removeEventListener(eventName, callbeck);
-    }
-
-    return this;
-}
-
-$.prototype.click  = function(hendlr){
-    for (let i = 0; i < this.length; i++){
-        if(hendlr){
-            this[i].addEventListener('click', hendlr);
-        } else {
-            this[i].click();
+$.prototype.html = function(content){
+    for(let i = 0; i < this.length; i++){
+        if(content){
+            this[i].innerHtml = content;
+        }else{
+            return this[i].innerHtml;
         }
     }
 
     return this;
+}
+
+$.prototype.eq = function(i){
+    const swap = this[i];
+    const oqjLength = Object.keys(this).length;
+    
+    for(let i = 0; i <  oqjLength; i++ ){
+        delete this[i];
+    }
+
+    this[0] = swap;
+    this.length = 1;
+    return this;
+}
+
+$.prototype.index = function(){
+    const parent = this[0].parentNode;
+    const сhilds = [...parent.children];
+
+    const findMyIndex = (item) =>{
+        return item == this[0];
+    };
+
+    return сhilds.findIndex(findMyIndex);
+}
+
+$.prototype.find = function(selector){
+     let numberOfItems = 0;
+     let counter = 0;
+
+     const copyObj = Object.assign({}, this);
+
+     for(let i = 0; i < copyObj.length; i++){
+        const arr = copyObj[i].querySelectorAll(selector);
+        if(arr.length == 0){
+            continue;
+        }
+
+        for(let j = 0;  j < arr.length; j++){
+            this[counter] = arr[j];
+            counter++;
+        }
+
+        numberOfItems += arr.length;
+     }
+
+     this.length = numberOfItems; 
+
+     for(; numberOfItems < objLength; numberOfItems++){
+        delete this[numberOfItems];
+     }
+
+     return this;
 }
---
+++ Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/hendlers.js
@@ -0,0 +1,35 @@
+import $  from "../core"; 
+
+$.prototype.on  = function(eventName, callbeck){
+    if(!eventName || !callbeck){
+        return this;
+    }
+    for (let i = 0; i < this.length; i++){
+        this[i].addEventListener(eventName, callbeck);
+    }
+
+    return this;
+}
+
+$.prototype.off  = function(eventName, callbeck){
+    if(!eventName || !callbeck){
+        return this;
+    }
+    for (let i = 0; i < this.length; i++){
+        this[i].removeEventListener(eventName, callbeck);
+    }
+
+    return this;
+}
+
+$.prototype.click  = function(hendlr){
+    for (let i = 0; i < this.length; i++){
+        if(hendlr){
+            this[i].addEventListener('click', hendlr);
+        } else {
+            this[i].click(); 
+        }
+    }
+
+    return this;
+}
