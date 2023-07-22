e0f7c9ba0c283c1de776f01e9a283ab4f4e465b6
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-07-22 20:00:46 +0300

    Creating methods for working with elements, part 2

--- Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/actions.js
+++ Section 4: Создаем свою JavaScript UI библиотеку/src/js/lib/modules/actions.js
@@ -1,66 +1,109 @@
-import $  from "./core";
+import $ from "./core";
 
-$.prototype.html = function(content){
-    for(let i = 0; i < this.length; i++){
-        if(content){
-            this[i].innerHtml = content;
-        }else{
-            return this[i].innerHtml;
-        }
+$.prototype.html = function (content) {
+  for (let i = 0; i < this.length; i++) {
+    if (content) {
+      this[i].innerHtml = content;
+    } else {
+      return this[i].innerHtml;
+    }
+  }
+
+  return this;
+};
+
+$.prototype.eq = function (i) {
+  const swap = this[i];
+  const oqjLength = Object.keys(this).length;
+
+  for (let i = 0; i < oqjLength; i++) {
+    delete this[i];
+  }
+
+  this[0] = swap;
+  this.length = 1;
+  return this;
+};
+
+$.prototype.index = function () {
+  const parent = this[0].parentNode;
+  const сhilds = [...parent.children];
+
+  const findMyIndex = (item) => {
+    return item == this[0];
+  };
+
+  return сhilds.findIndex(findMyIndex);
+};
+
+$.prototype.find = function (selector) {
+  let numberOfItems = 0;
+  let counter = 0;
+
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].querySelectorAll(selector);
+    if (arr.length == 0) {
+      continue;
     }
 
-    return this;
-}
-
-$.prototype.eq = function(i){
-    const swap = this[i];
-    const oqjLength = Object.keys(this).length;
-    
-    for(let i = 0; i <  oqjLength; i++ ){
-        delete this[i];
+    for (let j = 0; j < arr.length; j++) {
+      this[counter] = arr[j];
+      counter++;
     }
 
-    this[0] = swap;
-    this.length = 1;
-    return this;
-}
+    numberOfItems += arr.length;
+  }
 
-$.prototype.index = function(){
-    const parent = this[0].parentNode;
-    const сhilds = [...parent.children];
+  this.length = numberOfItems;
 
-    const findMyIndex = (item) =>{
-        return item == this[0];
-    };
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
 
-    return сhilds.findIndex(findMyIndex);
-}
+  return this;
+};
 
-$.prototype.find = function(selector){
-     let numberOfItems = 0;
-     let counter = 0;
+$.prototype.closest = function (selector) {
+  let counter = 0;
 
-     const copyObj = Object.assign({}, this);
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    counter++;
+  }
 
-     for(let i = 0; i < copyObj.length; i++){
-        const arr = copyObj[i].querySelectorAll(selector);
-        if(arr.length == 0){
-            continue;
-        }
+  const objLength = Object.keys(this).length;
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+};
 
-        for(let j = 0;  j < arr.length; j++){
-            this[counter] = arr[j];
-            counter++;
-        }
+$.prototype.sibling = function (selector) {
+  let numberOfItems = 0;
+  let counter = 0;
 
-        numberOfItems += arr.length;
-     }
+  const copyObj = Object.assign({}, this);
 
-     this.length = numberOfItems; 
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
 
-     for(; numberOfItems < objLength; numberOfItems++){
-        delete this[numberOfItems];
-     }
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+      this[counter] = arr[j];
+      counter++;
+    }
 
-     return this;
-}
+    numberOfItems += arr.length -1;
+  }
+
+  this.length = numberOfItems;
+
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
+  return this;
+};
